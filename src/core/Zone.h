eb8916c7261f72aaf656983e7b20de192a0a957e
lilelvis98 <quevil@msn.com>
2017-03-18 11:16:00 +0100

    Docu de Zone

--- src/core/Zone.h
+++ src/core/Zone.h
@@ -38,43 +38,97 @@ public:
      */
     Zone();
 
-    //crée une instance de zone en passant en paramètre les coordonnées de la salle actuelle et son nom
+    /**
+     * \brief Consctructeur avec salle actuelle et niveau.
+     * \param[in] posx : \a salle_actuelle_x pour la nouvelle instance.
+     * \param[in] posy : \a salle_actuelle_y pour la nouvelle instance.
+     * \param[in] niv : le niveau de la nouvelle Zone.
+     */
     Zone(int posx, int posy, int niv);
 
-    //constructeur depuis un modèle de génération de type ZoneGen
+    /**
+     * \brief Consctructeur depuis un generateur ZoneGen.
+     * \param[in] z : l'instance de ZoneGen qui sera traduite en Zone.
+     * Ne genere pas les instances de Salle, mais prend la \a config necessaire.
+     */
     Zone(const ZoneGen &z);
 
-    //renvoie la salle x,y
+    /**
+     * \brief Accesseur de Salle dans la \a carte.
+     * \param[in] x : coordonnees x de la Salle voulue dans la \a carte.
+     * \param[in] y : coordonnees y de la Salle voulue dans la \a carte.
+     * \return la Salle (x,y) de la \a carte.
+     */
     Salle get_salle(int x, int y);
 
-    //renvoie la salle actuelle
+    /**
+     * \brief Accesseur de Salle actuelle.
+     * \return la Salle actuelle.
+     */
     Salle get_salle();
 
-    //remplace la salle x,y par une salle passée en paramètre
+    /**
+     * \brief Mutateur de Salle de la \a carte.
+     * \param[in] x : les coordonnees x de la Salle à changer.
+     * \param[in] y : les coordonnees y de la Salle à changer.
+     * \param[in] nouvelle_salle : la Salle qui remplacera l'ancienne.
+     */
     void set_salle(int x, int y, const Salle &nouvelle_salle);
 
-    //renvoie la coordonnée x de la salle actuelle du joueur
+    /**
+     * \brief Accesseur de \a salle_actuelle_x
+     * \return \a salle_actuelle_x de l'instance.
+     */
     int get_salle_actuelle_x ()const;
 
-    //renvoie la coordonnée y de la salle actuelle du joueur
+    /**
+     * \brief Accesseur de \a salle_actuelle_y
+     * \return \a salle_actuelle_y de l'instance.
+     */
     int get_salle_actuelle_y ()const;
 
+    /**
+     * \brief Affiche la Zone (txt).
+     * Affiche la Zone dans la console en version txt.
+     */
     void afficher_zone();
 
-    //remplit la salle x,y (n'affecte que la booléenne associée)
+    /**
+     * \brief Appelle la procedure remplir() d'une Salle.
+     * \param[in] x : coordonnees x de la Salle à vider.
+     * \param[in] y : coordonnees y de la Salle à vider.
+     */
     void remplir_salle(int x, int y);
 
-    //remplit la salle actuelle (n'affecte que la booléenne associée)
+    /**
+     * \brief Appelle la procedure vider() de la Salle actuelle
+     * Sûrement temporaire et à supprimer plus tard.
+     */
     void remplir_salle();
 
-    //Vide la salle x,y
+    /**
+     * \brief Appelle la procedure vider() d'une Salle.
+     * \param[in] x : coordonnees x de la Salle à vider.
+     * \param[in] y : coordonnees y de la Salle à vider.
+     */
     void vider_salle(int x, int y);
 
     //Vide la salle actuelle (je vois pas pourquoi on s'en servirait mais eh...
     void vider_salle();
 
+    /**
+     * \brief Remplit les \a config de la \a carte.
+     * Crée un Modele à partir de \a nom et génère la Zone procéduralement à
+     * partir de ce Modele.
+     * \param[in] nom : le nom du Modele désiré pour la Zone.
+     */
     void zone_depuis_modele(std::string nom);
 
+    /**
+     * \brief Appelle zone_depuis_modele avec un nom aléatoire.
+     * Crée un Modele aléatoirement et génère la Zone procéduralement à
+     * partir de ce Modele.
+     */
     void zone_depuis_random_modele();
 };
 
