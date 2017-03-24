0e42b2eb47f4cc6d6e384a76b86f7ea46c313265
lilelvis98 <quevil@msn.com>
2017-03-24 13:07:46 +0100

    merge un peu nul

--- src/core/Projectile.h
+++ src/core/Projectile.h
@@ -1,6 +1,23 @@
 #ifndef PROJECTILE_H_INCLUDED
 #define PROJECTILE_H_INCLUDED
 
+#include "VecteurM.h"
+#include "Coord2D.h"
 
+class Projectile
+{
+private:
+    int id_projectile;
+
+    float degats;
+
+    VecteurM orientation, resultante;
+
+    Coord2D position, position_old;
+
+public:
+
+
+};
 
 #endif // PROJECTILE_H_INCLUDED
